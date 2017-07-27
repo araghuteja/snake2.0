63865c2f8f53e930b101ac6fc1d848d7cd4e1b5a
RaghuTeja A <a.raghu.teja@gmail.com>
2017-07-28 00:29:19 +0530

    fix

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ function draw() {
 	rect(px, px, (horz-2)*px, (vert-6)*px);
 	rect(px, (vert-4)*px, (horz-2)*px, 3*px);
 	fill(0);
-	text(screen.width + " x " + screen.height, 11*px, (2)*px);
 	if(food.createFood){
 		food.update();
 	}
